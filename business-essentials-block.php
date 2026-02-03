6dc4bd04f16ae40d448791548f9669732f264234
Moshtafizur-Rahman-Anty <alphaaa.90@gmail.com>
2026-02-03 19:25:52 +0600

    Initial plugin setup

---
+++ business-essentials-block.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Business Essentials Blocks
+ * Description: Hero, Team, and Products blocks for business websites.
+ * Version: 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
